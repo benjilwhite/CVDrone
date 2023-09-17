e5a495943256e61073a4992021741e84cbbb9e87
Benji White <blwhite@stanford.edu>
2023-09-17 13:26:58 -0400

    Add serial input format

--- src/RaspberryPi/ZoomFocusControl.py
+++ src/RaspberryPi/ZoomFocusControl.py
@@ -10,33 +10,46 @@ import os
 import sys
 import time
 import argparse
+import pigpio
 from Focuser import Focuser
-    
+
 def main():
 
     # establish a serial connection with the arduino
     # run the command "ls /dev/tty*" on the raspberry pi to see what port it is connected to
-    ser = serial.Serial('/dev/ttyUSB0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', baudrate = 115200, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout=1)
 
     # i2c_bus = 1
     focuser = Focuser(1)    
+    prevZoomValue = 0
+    prevFocusValue = 0
     
+    # Setup the gimbal controls
+    pan_pin = 14
+    tilt_pin = 15
+    pi = pigpio.pi()
+
     while True:
 
         # Wait for the next input
         while ser.in_waiting == 0 and focuser.isBusy:
             time.sleep(1)
 
+        # Split the inputs into a list
         line = ser.readline().decode('ascii').rstrip()
+        split_values = line.split(":")
+        values = [int(value) for value in split_values]
 
-        # TODO: figure out these mappings and how they will be encoded/decoded.
-        newZoomValue  = 0 # line[0:10]
-        newFocusValue = 0 # line[10:20]
-        newIRCut      = 0 # line[20:]
+        # update the gimbal position
+        # TODO: map the values to the correct servo values.
+        pi.set_servo_pulsewidth(pan_pin, values[0])
+        pi.set_servo_pulsewidth(tilt_pin, values[1])
+
+        newZoomValue  = values[2] # line[0:10]
+        newFocusValue = values[3] # line[10:20]
 
         focuser.set(Focuser.OPT_ZOOM, newZoomValue)
         focuser.set(Focuser.OPT_FOCUS, newFocusValue)
-        focuser.set(Focuser.OPT_IRCUT, newIRCut & 1)
 
         ser.reset_input_buffer() # clear out any inputs that have been received since the last update
     
